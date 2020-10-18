62e3c26eeffdecfbf38182365f2e6ab9a5da03dd
190050061-renu-k <70090832+190050061-renu-k@users.noreply.github.com>
2020-10-18 23:20:30 +0530

    Update form.component.ts

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,30 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { FormService } from '../form.service';
 import { FormBuilder, FormGroup } from '@angular/forms'
-import { Form } from '../form'
+import { detailed } from '../casting';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
+
 export class FormComponent implements OnInit {
   form: FormGroup;
+  model:detailed[] = [];
+  constructor(private fb: FormBuilder,public formService: FormService){
 
-  constructor(private fb: FormBuilder,public formService: FormService){}
-
-  submit(){
-	this.formService.postFormDetails(this.form.value).subscribe(data=>{
-	this.form=this.fb.group({
-		name:"",
-		email:"",
-		feedback:"",
-		comment:""
-	})
-	alert("Form submitted succesfully!");
-});
   }
-
-  ngOnInit(): void {
-  this.formService.getFormDetails().subscribe((data:Form)=>{this.form=this.fb.group(data);});}
-
+  submitted=false;
+  submit(){
+  if(this.form.value.name!="" && this.form.value.email!="" && this.form.value.feedback!=""){
+    this.model=this.form.value;
+	alert("Form submitted succesfully!");
+  }
+  else{
+    alert("Form invalid!");
+  }
+  
+  }
+  ngOnInit(): void{
+    this.formService.getDetails().subscribe(data => this.model = data);
+    this.formService.getFormDetails().subscribe(data =>{this.form=this.fb.group(data);});
+  }
 }
