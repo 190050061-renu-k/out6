0cc027fc6d60c9e1fa2ba3fd5ef482b9b19bf0e8
190050061-renu-k <70090832+190050061-renu-k@users.noreply.github.com>
2020-10-18 13:24:49 +0530

    Add files via upload

---
+++ src/app/form.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable()
+export class FormService {
+
+       constructor(private http:HttpClient) { }
+
+private handleError(error: HttpErrorResponse) {
+  if (error.error instanceof ErrorEvent) {
+    // A client-side or network error occurred. Handle it accordingly.
+    console.log('An error occurred:', error.error.message);
+  } else {
+    alert(
+      `Backend returned code ${error.status}, unsuccessful submission.`);
+  }
+  return throwError(
+    'Something bad happened, please try again later.');
+}
+
+getFormDetails(){
+	return this.http.get("https://cs251-outlab-6.herokuapp.com/initial_values/");
+	}
+
+postFormDetails(data){
+return this.http.post("https://cs251-outlab-6.herokuapp.com/add_new_feedback/",data).pipe(catchError(this.handleError));
+}
+
+}
